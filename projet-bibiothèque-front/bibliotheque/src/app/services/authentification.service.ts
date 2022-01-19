7691dca3d21d54d9e2461b077bd1bf0da2e32267
dallel mekkiou <dallelmk@hotmail.fr>
2022-01-19 15:11:07 +0100

    implémentation de l'algorithme de connexion

--- projet-bibiothèque-front/bibliotheque/src/app/services/authentification.service.ts
+++ projet-bibiothèque-front/bibliotheque/src/app/services/authentification.service.ts
@@ -1,3 +1,4 @@
+import { analytics } from '@angular-devkit/core';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -11,5 +12,18 @@ export class AuthentificationService {
     {nom:'anis',motPasse:'aaaa'}
 
   ];
+  user:any;
+  valide=false;
   constructor() { }
+
+ verifAuthentification(nom:any,motPasse:any){
+   for (this.user in this.users){
+     if(this.user.nom==nom){
+       if(this.user.motPasse==motPasse){this.valide=true}else{return "Mot de passe incorrect !"}
+     }else{ return " Nom incorrect !"}
+   }
+   return this.valide;
+
+ }
+
 }
