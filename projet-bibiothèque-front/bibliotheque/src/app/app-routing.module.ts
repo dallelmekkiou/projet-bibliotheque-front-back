57668556e8588663a40912df50d513a21c41761a
dallel mekkiou <dallelmk@hotmail.fr>
2022-01-19 14:33:15 +0100

    routing pour authentification

--- projet-bibiothèque-front/bibliotheque/src/app/app-routing.module.ts
+++ projet-bibiothèque-front/bibliotheque/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthentificationComponent } from './components/authentification/authentification.component';
 import { BibliothequeComponent } from './components/bibliotheque/bibliotheque.component';
 import { GestionBibliothequeComponent } from './components/gestion-bibliotheque/gestion-bibliotheque.component';
 import { HomeComponent } from './components/home/home.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   {path:'',component: HomeComponent},
   {path:'bibliotheque',component: BibliothequeComponent},
   {path:'gestionBibliotheque',component: GestionBibliothequeComponent},
+  {path:'authentification',component: AuthentificationComponent},
 
 
 ];
